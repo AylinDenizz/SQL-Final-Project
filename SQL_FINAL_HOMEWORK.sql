d1a66e121c746f4fa3d75f16c660e7f56ada87df
Aylin Deniz <aylinozunlu@gmail.com>
2023-06-15 14:46:06 +0300

    Create SQL_FINAL_HOMEWORK.sql

---
+++ SQL_FINAL_HOMEWORK.sql
@@ -0,0 +1,45 @@
+/*CREATE TABLE IF NOT EXISTS users (
+  user_id INT PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  creation_date DATE DEFAULT CURRENT_TIMESTAMP,
+  is_active BOOLEAN
+);
+
+
+
+CREATE TABLE IF NOT EXISTS posts (
+  post_id INT PRIMARY KEY,
+  user_id INT NOT NULL,
+  category_id INT NOT NULL,
+  title VARCHAR(50),
+  content TEXT,
+  view_count INT DEFAULT 0,
+  creation_date DATE DEFAULT CURRENT_TIMESTAMP,
+  is_published BOOLEAN,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+*/
+
+CREATE TABLE IF NOT EXISTS categories (
+  category_id INT PRIMARY KEY,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  creation_date DATE DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (creation_date) REFERENCES users(creation_date),
+  FOREIGN KEY (category_id) REFERENCES posts(category_id)
+);
+
+
+/*
+CREATE TABLE IF NOT EXISTS comments (
+  comment_id INT PRIMARY KEY,
+  post_id INT NOT NULL,
+  user_id INT NOT NULL,
+  comment TEXT NOT NULL,
+  creation_date DATE DEFAULT CURRENT_TIMESTAMP,
+  is_confirmed BOOLEAN,
+  FOREIGN KEY (post_id) REFERENCES posts(post_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (creation_date) REFERENCES categories(creation_date)
+);
+*/
