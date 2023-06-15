668436607308bc7dd782f3e524f0b2094cd63527
Aylin Deniz <aylinozunlu@gmail.com>
2023-06-15 17:21:03 +0300

    Comments table is updated with comment column values.

---
+++ SQL_INSERT_COMMENTS2.sql
@@ -0,0 +1,195 @@
+UPDATE comments SET comment = 'Great post! Really enjoyed reading it.'
+WHERE comment_id = 1;
+
+UPDATE comments SET comment = 'Great post! Thanks for sharing.'
+WHERE comment_id = 2;
+
+UPDATE comments SET comment = 'I completely agree with your point of view.'
+WHERE comment_id = 3;
+
+UPDATE comments SET comment = 'This article has been really helpful.'
+WHERE comment_id = 4;
+
+UPDATE comments SET comment = 'I have a different opinion on this topic.'
+WHERE comment_id = 5;
+
+UPDATE comments SET comment = 'Well-written and informative!'
+WHERE comment_id = 6;
+
+UPDATE comments SET comment = 'I enjoyed reading this post. Thanks for sharing.'
+WHERE comment_id = 7;
+
+UPDATE comments SET comment = 'This topic has always fascinated me.'
+WHERE comment_id = 8;
+
+UPDATE comments SET comment = 'Well-written and thought-provoking.'
+WHERE comment_id = 9;
+
+UPDATE comments SET comment = 'I have a different perspective on this subject.'
+WHERE comment_id = 10;
+
+UPDATE comments SET comment = 'Looking forward to more content like this!'
+WHERE comment_id = 11;
+
+UPDATE comments SET comment = 'Great post! Thanks for sharing.'
+WHERE comment_id = 12;
+
+UPDATE comments SET comment = 'I completely agree with your point of view.'
+WHERE comment_id = 13;
+
+UPDATE comments SET comment = 'This article has been really helpful.'
+WHERE comment_id = 14;
+
+UPDATE comments SET comment = 'I have a different opinion on this topic.'
+WHERE comment_id = 15;
+
+UPDATE comments SET comment = 'Well-written and informative!'
+WHERE comment_id = 16;
+
+UPDATE comments SET comment = 'Great article! It provided valuable insights.'
+WHERE comment_id = 17;
+
+UPDATE comments SET comment = 'Great article! It provided valuable insights.'
+WHERE comment_id = 18;
+
+UPDATE comments SET comment = 'I enjoyed reading this post. Thanks for sharing.'
+WHERE comment_id = 19;
+
+UPDATE comments SET comment = 'This topic has always fascinated me.'
+WHERE comment_id = 20;
+
+UPDATE comments SET comment = 'Well-written and thought-provoking.'
+WHERE comment_id = 21;
+
+UPDATE comments SET comment = 'I have a different perspective on this subject.'
+WHERE comment_id = 22;
+
+UPDATE comments SET comment = 'I completely agree with your point of view.'
+WHERE comment_id = 23;
+
+UPDATE comments SET comment = 'This article has been really helpful.'
+WHERE comment_id = 24;
+
+UPDATE comments SET comment = 'I have a different opinion on this topic.'
+WHERE comment_id = 25;
+
+UPDATE comments SET comment = 'Well-written and informative!'
+WHERE comment_id = 26;
+
+UPDATE comments SET comment = 'I enjoyed reading this post. Thanks for sharing.'
+WHERE comment_id = 27;
+
+UPDATE comments SET comment = 'This topic has always fascinated me.'
+WHERE comment_id = 28;
+
+UPDATE comments SET comment = 'Well-written and thought-provoking.'
+WHERE comment_id = 29;
+
+UPDATE comments SET comment = 'I have a different perspective on this subject.'
+WHERE comment_id = 30;
+
+UPDATE comments SET comment = 'Great post! Really enjoyed reading it.'
+WHERE comment_id = 31;
+
+UPDATE comments SET comment = 'Great post! Thanks for sharing.'
+WHERE comment_id = 32;
+
+UPDATE comments SET comment = 'I completely agree with your point of view.'
+WHERE comment_id = 33;
+
+UPDATE comments SET comment = 'This article has been really helpful.'
+WHERE comment_id = 34;
+
+UPDATE comments SET comment = 'I have a different opinion on this topic.'
+WHERE comment_id = 35;
+
+UPDATE comments SET comment = 'Well-written and informative!'
+WHERE comment_id = 36;
+
+UPDATE comments SET comment = 'I enjoyed reading this post. Thanks for sharing.'
+WHERE comment_id = 37;
+
+UPDATE comments SET comment = 'This topic has always fascinated me.'
+WHERE comment_id = 38;
+
+UPDATE comments SET comment = 'Well-written and thought-provoking.'
+WHERE comment_id = 39;
+
+UPDATE comments SET comment = 'Well-written and thought-provoking.'
+WHERE comment_id = 40;
+
+UPDATE comments SET comment = 'Great post! Really enjoyed reading it.'
+WHERE comment = '1A';
+
+UPDATE comments SET comment = 'Great post! Thanks for sharing.'
+WHERE comment = '2A';
+
+UPDATE comments SET comment = 'I completely agree with your point of view.'
+WHERE comment = '3A';
+
+UPDATE comments SET comment = 'This article has been really helpful.'
+WHERE comment = '4A';
+
+UPDATE comments SET comment = 'I have a different opinion on this topic.'
+WHERE comment = '5A';
+
+UPDATE comments SET comment = 'Well-written and informative!'
+WHERE comment = '6A';
+
+UPDATE comments SET comment = 'I enjoyed reading this post. Thanks for sharing.'
+WHERE comment = '7A';
+
+UPDATE comments SET comment = 'This topic has always fascinated me.'
+WHERE comment = '8A';
+
+UPDATE comments SET comment = 'Well-written and thought-provoking.'
+WHERE comment = '9A';
+
+UPDATE comments SET comment = 'I have a different perspective on this subject.'
+WHERE comment = '0A';
+
+
+UPDATE comments SET comment = 'Great article! It provided valuable insights.'
+WHERE comment_id = 42;
+
+UPDATE comments SET comment = 'I enjoyed reading this post. Thanks for sharing.'
+WHERE comment_id = 45;
+
+UPDATE comments SET comment = 'This topic has always fascinated me.'
+WHERE comment_id = 46;
+
+UPDATE comments SET comment = 'Well-written and thought-provoking.'
+WHERE comment_id = 50;
+
+UPDATE comments SET comment = 'I have a different perspective on this subject.'
+WHERE comment_id = 59;
+
+UPDATE comments SET comment = 'I completely agree with your point of view.'
+WHERE comment_id = 60;
+
+UPDATE comments SET comment = 'This article has been really helpful.'
+WHERE comment_id = 63;
+
+UPDATE comments SET comment = 'I have a different opinion on this topic.'
+WHERE comment_id = 64;
+
+UPDATE comments SET comment = 'Well-written and informative!'
+WHERE comment_id = 69;
+
+UPDATE comments SET comment = 'I enjoyed reading this post. Thanks for sharing.'
+WHERE comment_id = 71;
+
+UPDATE comments SET comment = 'This topic has always fascinated me.'
+WHERE comment_id = 77;
+
+UPDATE comments SET comment = 'Well-written and thought-provoking.'
+WHERE comment_id = 81;
+
+UPDATE comments SET comment = 'I have a different perspective on this subject.'
+WHERE comment_id = 83;
+
+UPDATE comments SET comment = 'Great post! Really enjoyed reading it.'
+WHERE comment_id = 86;
+
+UPDATE comments SET comment = 'Great post! Thanks for sharing.'
+WHERE comment_id = 96;
