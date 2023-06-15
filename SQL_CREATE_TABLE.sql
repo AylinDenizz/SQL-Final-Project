f500605b8d982da8f7c9817be2601d2ee24d7f49
Aylin Deniz <aylinozunlu@gmail.com>
2023-06-15 15:36:18 +0300

    CREATE_TABLE file changed

--- SQL_CREATE_TABLE.sql
+++ SQL_CREATE_TABLE.sql
@@ -1,12 +1,16 @@
 CREATE TABLE IF NOT EXISTS users (
-  user_id INT PRIMARY KEY,
+  user_id INT PRIMARY KEY UNIQUE,
   username VARCHAR(255) UNIQUE NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   creation_date DATE DEFAULT CURRENT_TIMESTAMP,
   is_active BOOLEAN
 );
 
-
+CREATE TABLE IF NOT EXISTS categories (
+  category_id INT PRIMARY KEY NOT NULL ,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  creation_date DATE 
+);
 
 CREATE TABLE IF NOT EXISTS posts (
   post_id INT PRIMARY KEY,
@@ -17,26 +21,10 @@ CREATE TABLE IF NOT EXISTS posts (
   view_count INT DEFAULT 0,
   creation_date DATE DEFAULT CURRENT_TIMESTAMP,
   is_published BOOLEAN,
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
 
-
-
-
-ALTER TABLE posts ADD UNIQUE (user_id);
-ALTER TABLE posts ADD UNIQUE (category_id);
-ALTER TABLE users ADD UNIQUE (creation_date);
-
-CREATE TABLE IF NOT EXISTS categories (
-  category_id INT PRIMARY KEY NOT NULL,
-  name VARCHAR(255) UNIQUE NOT NULL,
-  creation_date DATE ,
-  FOREIGN KEY (category_id) REFERENCES posts(category_id),
-  FOREIGN KEY (creation_date) REFERENCES users(creation_date)
-);
-
-
-ALTER TABLE categories ADD UNIQUE (creation_date);
 
 CREATE TABLE IF NOT EXISTS comments (
   comment_id INT PRIMARY KEY,
@@ -46,8 +34,5 @@ CREATE TABLE IF NOT EXISTS comments (
   creation_date DATE DEFAULT CURRENT_TIMESTAMP,
   is_confirmed BOOLEAN,
   FOREIGN KEY (post_id) REFERENCES posts(post_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (creation_date) REFERENCES categories(creation_date)
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
-
