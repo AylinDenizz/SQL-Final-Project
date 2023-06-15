aae6b4673a3b29ec85d756d72d12f2671084a8b7
Aylin Deniz <aylinozunlu@gmail.com>
2023-06-16 00:03:17 +0300

    all the query has been created exceot 24 and 25

--- SQL_OPERATİONS.sql
+++ SQL_OPERATİONS.sql
@@ -65,15 +65,72 @@ JOIN posts ON users.user_id = posts.user_id
 WHERE posts.title = 'Red Dead Redemption 2' ;
 
 --13. Başlık veya içeriklerinde belirli bir anahtar kelime bulunan tüm gönderileri bulun.
+SELECT * FROM posts
+WHERE title LIKE  '%rd%' AND content LIKE '%rd%';
+
 --14. Belirli bir kullanıcının en son yorumunu gösterin.
+SELECT comments.comment, users.username FROM comments
+JOIN users ON users.user_id = comments.user_id
+WHERE users.username = 'kgori0'
+ORDER BY comments.creation_date
+LIMIT 1;
+
 --15. Gönderi başına ortalama yorum sayısını bulun.
+SELECT ROUND(AVG(count_posts),0) FROM (
+	SELECT COUNT(comment) AS count_posts, post_id FROM comments
+	GROUP BY post_id) AS count_table;
+
 --16. Son 30 günde yayınlanan gönderileri gösterin.
+SELECT *
+FROM posts
+WHERE is_published IS TRUE
+  AND creation_date BETWEEN CURRENT_DATE AND '2023-05-15';
+
 --17. Belirli bir kullanıcının yaptığı yorumları alın.
+SELECT users.username,comments.comment FROM users
+JOIN comments ON users.user_id = comments.user_id
+WHERE users.username = 'kgori0' ;
+
 --18. Belirli bir kategoriye ait tüm gönderileri bulun.
+SELECT posts.title, categories.name FROM categories
+JOIN posts ON categories.category_id= posts.category_id
+WHERE categories.name = 'Music' ;
+
 --19. 5'ten az yazıya sahip kategorileri bulun.
+SELECT posts.content, categories.name FROM categories
+JOIN posts ON categories.category_id= posts.category_id
+GROUP BY categories.name, posts.content
+HAVING COUNT(posts.content) < 5 ;
+
 --20. Hem bir yazı hem de bir yoruma sahip olan kullanıcıları gösterin.
+SELECT users.user_id, users.username
+FROM users 
+WHERE EXISTS ( SELECT 1 FROM posts 
+    WHERE posts.user_id = users.user_id
+) AND EXISTS ( SELECT 1 FROM comments 
+    WHERE comments.user_id = users.user_id
+);
+
 --21. En az 2 farklı yazıya yorum yapmış kullanıcıları alın.
+SELECT DISTINCT user_id FROM comments
+GROUP BY user_id
+HAVING COUNT(DISTINCT post_id) >= 2;
+
 --22. En az 3 yazıya sahip kategorileri görüntüleyin.
+SELECT category_id, COUNT(*) as post_count FROM posts
+GROUP BY category_id
+HAVING COUNT(*) >= 3;
+
 --23. 5'ten fazla blog yazısı yazan yazarları bulun.
+SELECT user_id, COUNT(*) as post_count FROM posts
+GROUP BY user_id
+HAVING COUNT(*) > 5;
+
 --24. Bir blog yazısı yazmış veya bir yorum yapmış kullanıcıların e-posta adreslerini görüntüleyin. (UNION kullanarak)
+
+
 --25. Bir blog yazısı yazmış ancak hiç yorum yapmamış yazarları bulun.
+
+
+
+
