28bb4529b86d9b01bed5678c9cc4fc5a629c7788
Aylin Deniz <aylinozunlu@gmail.com>
2023-06-16 14:50:19 +0300

    INSERT folder added

---
+++ INSERT/SQL_INSERT_POSTS2.sql
@@ -0,0 +1,56 @@
+UPDATE posts SET title = 'The Great Gatsby',
+content = 'Experience the glamour and tragedy of the Jazz Age in F. Scott Fitzgeralds masterpiece, -The Great Gatsby.- Follow Jay Gatsbys pursuit of the American Dream in this iconic novel.'
+WHERE post_id = 4;
+
+UPDATE posts SET title = 'Pride and Prejudice',
+content = 'Delve into Jane Austens timeless classic, -Pride and Prejudice,- a story of love, societal expectations, and personal growth. Join Elizabeth Bennet on her journey to find true happiness.'
+WHERE post_id = 7;
+
+UPDATE posts SET title = 'To Kill a Mockingbird',
+content = 'Harper Lees -To Kill a Mockingbird- explores themes of racial injustice and moral courage through the eyes of Scout Finch. Discover the power of empathy and the importance of standing up for what is right.'
+WHERE post_id = 14;
+
+UPDATE posts SET title = '1984',
+content = 'George Orwells dystopian novel, -1984,- paints a chilling portrait of a totalitarian society where Big Brother watches everyone. Dive into this thought-provoking exploration of power, surveillance, and freedom.'
+WHERE post_id = 17;
+
+UPDATE posts SET title = 'The Catcher in the Rye',
+content = 'Join Holden Caulfield on his journey of self-discovery and rebellion in J.D. Salingers -The Catcher in the Rye.- Explore themes of alienation, identity, and the loss of innocence.'
+WHERE post_id = 24;
+
+UPDATE posts SET title = 'Moby-Dick',
+content = 'Embark on a thrilling adventure with Captain Ahab in Herman Melvilles epic tale, -Moby-Dick.- Explore the depths of obsession, fate, and the power of nature in this literary masterpiece.'
+WHERE post_id = 34;
+
+UPDATE posts SET title = 'Jane Eyre',
+content = 'Charlotte Brontës -Jane Eyre- is a hauntingly beautiful novel that follows the life of the independent and passionate Jane. Discover themes of love, identity, and social class in this timeless classic.'
+WHERE post_id = 42;
+
+UPDATE posts SET title = 'The Odyssey',
+content = 'Journey alongside the legendary hero Odysseus in Homers epic poem, -The Odyssey.- Experience the trials, adventures, and mythical creatures that await him on his quest to return home.'
+WHERE post_id = 47;
+
+UPDATE posts SET title = 'Brave New World',
+content = 'Aldous Huxleys -Brave New World- presents a dystopian future where technology, consumerism, and conformity rule. Explore themes of individuality, societal control, and the price of happiness.'
+WHERE post_id = 49;
+
+UPDATE posts SET title = 'Crime and Punishment',
+content = 'Delve into Fyodor Dostoevskys psychological masterpiece, -Crime and Punishment.- Follow the troubled Raskolnikov as he grapples with guilt, redemption, and the consequences of his actions.'
+WHERE post_id = 50;
+
+UPDATE posts SET title = 'Wuthering Heights',
+content = 'Emily Brontës -Wuthering Heights- is a passionate tale of love, revenge, and the haunting power of the Yorkshire moors. Immerse yourself in this Gothic masterpiece of literature.'
+WHERE post_id = 54;
+
+UPDATE posts SET title = 'The Lord of the Rings',
+content = 'Enter the enchanting world of Middle-earth in J.R.R. Tolkiens epic fantasy trilogy, -The Lord of the Rings.- Join Frodo Baggins on his perilous quest to destroy the One Ring and save Middle-earth from darkness.'
+WHERE post_id = 55;
+
+UPDATE posts SET title = 'The Picture of Dorian Gray',
+content = 'Oscar Wildes -The Picture of Dorian Gray- explores the pursuit of eternal youth and the consequences of living a life solely dedicated to pleasure. Dive into this provocative and philosophical novel.'
+WHERE post_id = 56;
+
+UPDATE posts SET title = 'The Alchemist',
+content = 'Paulo Coelhos -The Alchemist- is a mesmerizing tale of self-discovery and following ones dreams. Join Santiago on his journey through the desert as he learns valuable lessons about life and destiny.'
+WHERE post_id = 59;
+
---
+++ INSERT/SQL_INSERT_categories.sql
@@ -0,0 +1,5 @@
+insert into categories (category_id, name, creation_date) values (1, 'Music', '2018-06-14');
+insert into categories (category_id, name, creation_date) values (2, 'Art', '2021-12-23');
+insert into categories (category_id, name, creation_date) values (3, 'Movie', '2022-05-23');
+insert into categories (category_id, name, creation_date) values (4, 'Game', '2021-07-15');
+insert into categories (category_id, name, creation_date) values (5, 'Literature', '2019-07-04');
